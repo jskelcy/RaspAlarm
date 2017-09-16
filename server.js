28e169bbf1bd666b23cdfbdc0bdb23aababbd718
Steve <sfreeman422@gmail.com>
2017-09-16 19:55:05 -0400

    Adjusted further linting. Confirmed that all features are functional. Working on the settings pane and options

--- server.js
+++ server.js
@@ -7,73 +7,72 @@ const methodOverride = require('method-override');
 const fs = require('fs');
 
 const app = express();
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 3000;
 
-//Connect to mongo and let us know that we are successfully connected or there was an error. 
-mongoose.connect("mongodb://localhost:27017/alarms");
+// Connect to mongo and let us know that we are successfully connected or there was an error.
+mongoose.connect('mongodb://localhost:27017/alarms');
 const db = mongoose.connection;
-db.on("open", function(){
-	console.log("Connected to MongoDB on port 27017.");
+db.on('open', () => {
+  console.log('Connected to MongoDB on port 27017.');
 });
-db.on("error", function(err){
-	console.log("An error has occurred: "+err);
-})
+db.on('error', (err) => {
+  console.log(`An error has occurred: ${err}`);
+});
 
 app.use(logger('dev'));
 app.use(bodyParser());
 app.use(express.static('./public'));
 app.use(methodOverride('_method'));
 
-//Initial route to load the page for the Timer, weather information, etc. 
-app.get('/', function(req, res){
-	res.sendFile('./public/index.html');
+// Initial route to load the page for the Timer, weather information, etc.
+app.get('/', (req, res) => {
+  res.sendFile('./public/index.html');
 });
-//Route to grab the set alarms from the mongoDB. 
-app.get('/alarms', function(req, res){
-	//Mongoose method to retrieve all 
-	Alarm.find({}, function(err, docs){
-		if(!err && docs){
-				res.send(docs);
-		}
-		else{
-			throw err;
-		}
-	});
+// Route to grab the set alarms from the mongoDB.
+app.get('/alarms', (req, res) => {
+  // Mongoose method to retrieve all
+  Alarm.find({}, (err, docs) => {
+    if (!err && docs) {
+      res.send(docs);
+    } else {
+      throw err;
+    }
+  });
 });
-//Get image file names for use in the settingsModal
-app.get('/images', function(req, res){
-	const allImages = []; 
-	fs.readdir("./public/bgs/", (err, files)=>{
-		files.forEach(file =>{
-			allImages.push(file);
-		});
-		res.send(allImages);
-	});
-});
-
-//Route to set alarms. 
-app.post('/setAlarm', function(req, res){
-	console.log(req.body);
-	var userTime = req.body.hour + ":" + req.body.minute + req.body.ampm;
-	var newAlarm = new Alarm({
-		time: userTime,
-		dayOfWeek: req.body.dayOfWeek
-	});
-
-	newAlarm.save(function(err, completed){
-		if(err)throw err;
-		console.log("Alarm saved as : "+completed);
-	})
-	res.end("Success");
+// Get image file names for use in the settingsModal
+app.get('/images', (req, res) => {
+  const allImages = [];
+  fs.readdir('./public/bgs/', (err, files) => {
+    files.forEach((file) => {
+      allImages.push(file);
+    });
+    res.send(allImages);
+  });
 });
 
-//Route to delete alarms
-app.delete('/deleteAlarm', function(req, res){
-	console.log(req.body);
-	Alarm.find({_id: req.body.id}).remove(function(){console.log("Successfully removed.")});
-	res.send("Success");
-})
-//Listen to the port.
-app.listen(PORT, function(){
-	console.log('listening on port '+PORT);
+// Route to set alarms.
+app.post('/setAlarm', (req, res) => {
+  console.log(req.body);
+  const userTime = `${req.body.hour}:${req.body.minute}${req.body.ampm}`;
+  const newAlarm = new Alarm({
+    time: userTime,
+    dayOfWeek: req.body.dayOfWeek,
+  });
+
+  newAlarm.save((err, completed) => {
+    if (err) throw err;
+    console.log(`Alarm saved as : ${completed}`);
+  });
+  res.end('Success');
+});
+
+// Route to delete alarms
+app.delete('/deleteAlarm', (req, res) => {
+  console.log(req.body);
+  Alarm.find({ _id: req.body.id }).remove(() => { console.log('Successfully removed.'); });
+  res.send('Success');
+});
+// Listen to the port.
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
 });
